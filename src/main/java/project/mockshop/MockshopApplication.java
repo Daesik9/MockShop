bed360d6cd4a4acb08b48ec7e843bc80cc38f0b1
Sykomic <seafood0917@gmail.com>
2024-06-14 11:32:22 +0900

    프로젝트 설정

---
+++ src/main/java/project/mockshop/MockshopApplication.java
@@ -0,0 +1,13 @@
+package project.mockshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/mockshop/MockshopApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mockshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
