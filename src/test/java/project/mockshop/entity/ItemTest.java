d735e770221335c2e0975b5dd1aae5fc39717e57
Sykomic <seafood0917@gmail.com>
2024-06-24 22:22:55 +0900

    ItemTest 추가

---
+++ src/test/java/project/mockshop/entity/ItemTest.java
@@ -0,0 +1,153 @@
+package project.mockshop.entity;
+
+import lombok.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+import project.mockshop.policy.MockShopPolicy;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 상품명, 카테고리, 상품 이미지, 가격, 총 재고, 상세 설명 이미지, 할인율을 첨부하면 상품을 등록할 수 있다.
+ * 상품명, 가격, 총 재고는 필수로 입력해야한다.
+ * 가격은 0보다 같거나 작을 수 없다.
+ * 재고는 0보다 같거나 작을 수 없다.
+ * 상품을 등록하면 판매자 PK가 자동으로 설정된다.
+ * 상품 번호는 자동으로 부여한다.
+ */
+public class ItemTest {
+    @Test
+    void itemNotNull() throws Exception {
+        //given
+        Item item;
+
+        //when
+        item = Item.builder().build();
+
+        //then
+        assertThat(item).isNotNull();
+    }
+
+    @Test
+    void createItem() throws Exception {
+        //given
+        Item item = Item.builder()
+                .id(1L)
+                .name("name")
+                .category("Book")
+                .thumbnail("image.png")
+                .price(1000)
+                .quantity(100)
+                .descriptionImg1("img1.png")
+                .descriptionImg2("img2.png")
+                .descriptionImg3("img3.png")
+                .percentOff(5)
+                .merchantId(1L)
+                .build();
+
+        //when
+
+        //then
+        assertThat(item.name).isEqualTo("name");
+    }
+
+    @Test
+    void createItem_fail_noName() throws Exception {
+        //given
+
+        //when
+        assertThatThrownBy(() -> Item.builder()
+                .name("")
+                .price(1000)
+                .quantity(100)
+                .build()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MockShopPolicy.INPUT_STRING_METHOD("상품명"));
+
+        //then
+    }
+
+    @Test
+    void createItem_fail_zeroPriceOrLess() throws Exception {
+        //given
+
+        //when
+        assertThatThrownBy(() -> Item.builder()
+                .name("상품명")
+                .price(0)
+                .quantity(100)
+                .build()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MockShopPolicy.INPUT_STRING_METHOD("상품 가격"));
+
+        assertThatThrownBy(() -> Item.builder()
+                .name("상품명")
+                .price(-100)
+                .quantity(100)
+                .build()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MockShopPolicy.INPUT_STRING_METHOD("상품 가격"));
+
+        //then
+    }
+
+    @Test
+    void createItem_fail_zeroQuantityOrLess() throws Exception {
+        //given
+
+        //when
+        assertThatThrownBy(() -> Item.builder()
+                .name("상품명")
+                .price(100)
+                .quantity(0)
+                .build()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MockShopPolicy.INPUT_STRING_METHOD("상품 재고"));
+
+        assertThatThrownBy(() -> Item.builder()
+                .name("상품명")
+                .price(100)
+                .quantity(-100)
+                .build()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MockShopPolicy.INPUT_STRING_METHOD("상품 재고"));
+
+        //then
+    }
+
+    @Builder
+    private static class Item {
+        private Long id;
+        private String name;
+        private String category;
+        private String thumbnail;
+        private int price;
+        private int quantity;
+        private String descriptionImg1;
+        private String descriptionImg2;
+        private String descriptionImg3;
+        private double percentOff;
+        private Long merchantId;
+
+        private static class ItemBuilder {
+            private ItemBuilder name(String name) {
+                if (!StringUtils.hasText(name)) {
+                    throw new IllegalArgumentException(MockShopPolicy.INPUT_STRING_METHOD("상품명"));
+                }
+                this.name = name;
+                return this;
+            }
+
+            private ItemBuilder price(int price) {
+                if (price <= 0) {
+                    throw new IllegalArgumentException(MockShopPolicy.INPUT_STRING_METHOD("상품 가격"));
+                }
+                this.price = price;
+                return this;
+            }
+
+            private ItemBuilder quantity(int quantity) {
+                if (quantity <= 0) {
+                    throw new IllegalArgumentException(MockShopPolicy.INPUT_STRING_METHOD("상품 재고"));
+                }
+                this.quantity = quantity;
+                return this;
+            }
+        }
+    }
+}
