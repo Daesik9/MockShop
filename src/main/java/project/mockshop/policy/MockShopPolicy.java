05dc3462124d081822a755f4e70dab7c1325d584
Sykomic <seafood0917@gmail.com>
2024-06-29 21:17:09 +0900

    input 알림 문구 변경

--- src/main/java/project/mockshop/policy/MockShopPolicy.java
+++ src/main/java/project/mockshop/policy/MockShopPolicy.java
@@ -11,7 +11,7 @@ public class MockShopPolicy {
 
     /**
      * @param input
-     * @return ${input}을/를 입력해주세요.
+     * @return ${input} 입력을 다시 확인해주세요.
      */
     public static String INPUT_STRING_METHOD(String input) {
         return input + " 입력을 다시 확인해주세요.";
