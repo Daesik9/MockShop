74518ac492ae975e21b5f13174a5ac13864a0f1d
Sykomic <seafood0917@gmail.com>
2024-10-05 12:47:40 +0900

    FEAT: 회원가입시 비밀번호 인코딩 기능 추가

--- src/main/java/project/mockshop/service/AuthService.java
+++ src/main/java/project/mockshop/service/AuthService.java
@@ -1,6 +1,7 @@
 package project.mockshop.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import project.mockshop.dto.LoginRequestDto;
 import project.mockshop.entity.Member;
@@ -14,22 +15,17 @@ import java.util.Optional;
 public class AuthService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public String login(LoginRequestDto loginRequestDto) {
-        Optional<Member> findMember = memberRepository.findByLoginId(loginRequestDto.getLoginId());
-
-        //아이디가 존재하지 않을 때
-        if (findMember.isEmpty()) {
-            throw new NullPointerException("아이디나 비밀번호가 일치하지 않습니다.");
-        }
+        Member findMember = memberRepository.findByLoginId(loginRequestDto.getLoginId())
+                .orElseThrow(() -> new NullPointerException("아이디나 비밀번호가 일치하지 않습니다."));
 
         //비밀번호가 일치하지 않을 때
-        if (!findMember.get().getPassword().equals(loginRequestDto.getPassword())) {
+        if (!bCryptPasswordEncoder.matches(loginRequestDto.getPassword(), findMember.getPassword())) {
             throw new NullPointerException("아이디나 비밀번호가 일치하지 않습니다.");
         }
 
-
-
-        return jwtTokenProvider.createToken(loginRequestDto.getLoginId(), findMember.get().getRole().toString(), findMember.get().getId().toString());
+        return jwtTokenProvider.createToken(loginRequestDto.getLoginId(), findMember.getRole(), findMember.getId().toString());
     }
 }
