4374e09fb99efc037a297d0f8de53f0406b6566e
Sykomic <seafood0917@gmail.com>
2024-11-13 22:00:12 +0900

    FIX: 성능향상을 위해 로그인시 전체 Member 데이터 조회에서 로그인에 필요한 데이터만 DTO로 가져오도록 수정

--- src/main/java/project/mockshop/repository/MemberRepository.java
+++ src/main/java/project/mockshop/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package project.mockshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import project.mockshop.dto.LoginDto;
 import project.mockshop.entity.Customer;
 import project.mockshop.entity.Member;
 
@@ -9,6 +11,9 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     //    Optional<Customer> findByLoginId(String loginId);
     Optional<Member> findByLoginId(String loginId);
+
+    @Query("SELECT new project.mockshop.dto.LoginDto(m.id, m.password, m.role) FROM Member m WHERE m.loginId = :loginId")
+    Optional<LoginDto> findLoginDtoByLoginId(String loginId);
 
     Optional<Customer> findLoginIdByPhoneNumber(String phoneNumber);
 
